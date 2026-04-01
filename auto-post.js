7dac5233e1c951b10f07ec42dbd889b6bee01b65
UnDecrypted <155349824+UnDecrypted@users.noreply.github.com>
2026-04-01 22:40:06 +0700

    Create auto-post.js

---
+++ auto-post.js
@@ -0,0 +1,28 @@
+// config
+const MESSAGE = "Test";
+const DELAY = 2000; // 2 detik (jangan terlalu cepat, nanti kena limit)
+
+async function sendMessage() {
+  try {
+    const res = await fetch("https://discord.com/api/v9/channels/1234263726052343878/messages", {
+      method: "POST",
+      headers: {
+        "authorization" : "OTYxOTYwMTQxOTgzNDQ5MTA4.GI_SHG.xiBeaWjclBfZhFnrMo4a6Is3vjBYPJnjqPzqPE"
+      },
+      body: {
+        content: MESSAGE
+      }
+    });
+
+    if (!res.ok) {
+      console.log("Kena limit / error");
+    } else {
+      console.log("Terkirim");
+    }
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+}
+
+// loop
+setInterval(sendMessage, DELAY);
